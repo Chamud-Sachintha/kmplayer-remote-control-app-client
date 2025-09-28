7e4c6fda9dac304a4626da2e9f694f3efeb7eacd
Chamud Sachintha <sheraanmario777@gmail.com>
2025-09-28 20:00:49 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'remote-key-control',
+  webDir: 'www'
+};
+
+export default config;
