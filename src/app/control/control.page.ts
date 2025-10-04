6a7f51bfadae619f06b5f6644706f416f24d830f
Chamud Sachintha <sheraanmario777@gmail.com>
2025-10-04 14:05:22 +0530

    add version 5 changes
    brithness control

--- src/app/control/control.page.ts
+++ src/app/control/control.page.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
   IonButton,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonLabel,
+  IonRange,
 } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { Http } from '@capacitor-community/http';
@@ -14,10 +20,11 @@ import { Http } from '@capacitor-community/http';
   templateUrl: 'control.page.html',
   styleUrls: ['control.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFab, IonFabButton, IonIcon, IonLabel, IonRange, FormsModule],
 })
 export class ControlPage {
   port = 5000;
+  brightness = 50;
   connectedIp: string | null = localStorage.getItem('connectedIp'); // saved from HomePage
 
   constructor(private router: Router) {}
@@ -43,6 +50,22 @@ export class ControlPage {
     } catch (err) {
       console.error('Error sending command', err);
       alert('Failed to send command');
+    }
+  }
+
+  async onBrightnessChange(event: any) {
+    const value = event.detail.value;
+    this.brightness = value;
+
+    try {
+      await Http.post({
+        url: `http://${this.connectedIp}:5000/brightness`,
+        headers: { 'Content-Type': 'application/json' },
+        data: { value },
+        params: {},
+      });
+    } catch (err) {
+      console.error('Error adjusting brightness:', err);
     }
   }
 }
